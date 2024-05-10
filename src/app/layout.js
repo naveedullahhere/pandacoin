fbed6f80f7b5e3cab70128b1581b27102f026635
naveedullahhere-INLP <naveed.ullah@innovative-net.com>
2024-05-11 03:49:49 +0500

    rrrr

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Footer from "./components/Layout/Footer";
 import Header from "./components/Layout/Header";
 import '../app/globals.css'
+import Script from "next/script";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"  />
       </head>
       <body>
         <Header />
