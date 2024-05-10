67784a3b5d5739b967b057ef70eeb6495beb9caf
naveedullahhere-INLP <naveed.ullah@innovative-net.com>
2024-05-11 03:22:07 +0500

    Initial

---
+++ src/app/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return  <footer className='text-center'>
+      <img className='footer-logo' src="/logo-header.png" alt="Panda" />
+        <div className="copyright">
+          <p className='m-0 py-3'>©2024 PANDACOIN, All Right Reserved.</p>
+        </div>
+</footer>;
+};
+
+export default Footer;
---
+++ src/app/components/Layout/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+        <img className='header-logo' src="/logo-header.png" alt="Panda" />
+        </a>
+        <button class="d-md-none  headerbtn me-2 tranlateeffect" type="button">Buy Now</button>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"> Where to buy</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"> Socials</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"> Tokenomics</a>
+            </li>
+          </ul>
+        
+         
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
